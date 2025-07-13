149c8856d77d90d774ea9359ee47fddcf47262ff
BharathSunkarKumar <155315751+BharathSunkaraKumar@users.noreply.github.com>
2025-07-14 03:10:34 +0530

    add search feature to my trendhut ecommerce web app

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useGetAllProductsQuery } from '../services/productsApi'
 import { Link } from 'react-router-dom'
 
 export default function Products() {
     const {data: products=[], isLoading} = useGetAllProductsQuery()
+    const [searchTerm, setSerchTerm] = useState('');
+
+   const filteredProducts = products.filter((e) => {
+        let term = searchTerm.toLowerCase();
+        let productTitle = e.title.toLowerCase().includes(term);
+        const description = e.description 
+        ? e.description.toLowerCase().includes(term) 
+        : false;
+        return productTitle || description
+   })
+    
   return (
-    <div className='mb-6 flex justify-center'>
+    <div className='mb-6 flex justify-center flex-col'>
+        <div className='mb-5 flex justify-center flex-col items-center gap-3'>
+            <h1 className='font-bold text-2xl'>All Products</h1>
+            <input 
+            class="w-full max-w-md rounded border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            type='text' 
+            placeholder='search items'
+            value={searchTerm}
+            onChange={(e) => {setSerchTerm(e.target.value)}}
+            />
+        </div>
         {
             isLoading ? <h3>Loading...</h3> : (
             <ul className='mt-6 grid grid-cols-1 gap-4 sm:grid-col-2 lg:grid-cols-3'>
+        
             {
-                products && products.map((each) => {
-                    // console.log(each)
+                filteredProducts.map((each) => {
+                    
                     return (
                         <li key={each.id} 
                         className='border border-gray-300 rounded-xl group hover:shadow-2xl transition duration-300 py-10 p-10 h-80 overflow-hidden flex flex-col'
